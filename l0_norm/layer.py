717d569bb0b4a69fbed1948c68f85204bba12998
daib13 <daib09physics@gmail.com>
2017-12-20 01:20:13 +0800

    implement lenet300

--- l0_norm/layer.py
+++ l0_norm/layer.py
@@ -6,26 +6,29 @@ import math
 PROTECTOR = 0.00001
 
 
-def dense_l0(name, x, output_channel, reg=None, bias=False, init_dropout_ratio=0.5, beta=2.0/3.0, gamma=-0.1, zeta=1.1):
+def dense_l0(name, x, output_channel, reg=None, bias=False, init_dropout_ratio=0.5, beta=2.0/3.0, gamma=-0.1, zeta=1.1, activation_fn=None):
     input_channel = int(x.get_shape()[-1])
     assert len(x.get_shape()) == 2
     beta_inv = 1.0 / beta
     bgz = -beta * math.log(-gamma / zeta)
     with tf.variable_scope(name + '_w'):
         w = tf.get_variable('w', [input_channel, output_channel], tf.float32, layers.xavier_initializer(), reg)
-        log_alpha = tf.get_variable('log_alpha', [input_channel, output_channel], tf.float32, tf.random_normal_initializer(tf.log(init_dropout_ratio / (1.0 - init_dropout_ratio)), 0.01))
+        log_alpha = tf.get_variable('log_alpha', [input_channel, 1], tf.float32, tf.random_normal_initializer(tf.log(init_dropout_ratio / (1.0 - init_dropout_ratio)), 0.01))
         if bias:
             b = tf.get_variable('b', [output_channel], tf.float32, tf.zeros_initializer())
     with tf.name_scope(name):
-        u = tf.random_uniform([input_channel, output_channel], PROTECTOR, 1.0 - PROTECTOR, tf.float32, name='u')
+        u = tf.random_uniform([input_channel, 1], PROTECTOR, 1.0 - PROTECTOR, tf.float32, name='u')
         alpha = tf.minimum(PROTECTOR, tf.exp(log_alpha), 'alpha')
         s = tf.divide(1.0, 1.0 + tf.pow((1.0 - u)/u/alpha, beta_inv), name='s')
         s_bar = s * (zeta - gamma) + gamma
         z = tf.minimum(tf.maximum(s_bar, 0.0), 1.0, 'z')
-        w_gated = tf.multiply(w, z, 'w_gated')
+        z_tile = tf.tile(z, [1, output_channel], 'z_tile')
+        w_gated = tf.multiply(w, z_tile, 'w_gated')
         if bias:
             y = tf.nn.bias_add(tf.matmul(x, w_gated), b, name='output')
         else:
             y = tf.matmul(x, w_gated, name='output')
+        if activation_fn is not None:
+            y = activation_fn(y, name='output_activate')
         l0_penalty = tf.nn.sigmoid(log_alpha + bgz, 'l0_penalty')
     return y, l0_penalty
--- l0_norm/lenet.py
+++ l0_norm/lenet.py
@@ -1,1 +1,18 @@
 import tensorflow as tf
+from layer import dense_l0
+
+
+class lenet300:
+    def __init__(self, init_dropout_ratio=0.5, beta=0.66667, gamma=-0.1, zeta=1.1):
+        self.init_dropout_ratio = init_dropout_ratio
+        self.beta = beta
+        self.gamma = gamma
+        self.zeta = zeta
+
+        self.x = tf.placeholder(tf.float32, [None, 784], name='x')
+        self.h1, self.penalty1 = dense_l0('layer1', self.x, 300, activation_fn=tf.nn.relu)
+        self.h2, self.penalty2 = dense_l0('layer2', h1, 100, activation_fn=tf.nn.relu)
+        self.y_logit, self.penalty3 = dense_l0('layer3', h2, 10)
+        self.y = tf.placeholder(tf.float32, [None, 10], name='y')
+
+        self.loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.y, logits=self.y_logit, name='loss')
